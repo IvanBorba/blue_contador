69848e4f8a0c4323766813ebbf31a0582f03db27
Ivan Borba <mrborbaivan@gmail.com>
2022-06-03 20:15:21 -0300

    feature: Primeira dinâmica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+// import React from "react";
 
-function App() {
+const App = () => {
+  const [number, setNumber] = useState(0);
+  // const [number, setNumber] = React.useState(0);
+
+  const addOne = () => {
+    setNumber(number + 1);
+  };
+
+  const removeOne = () => {
+    setNumber(number - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{number}</h1>
+      <div>
+        <button onClick={removeOne}>-</button>
+        <button onClick={addOne}>+</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+// Lista de passos pra criação da aplicação da dinâmica 1:
+// X Criar o projeto com o npx;
+// X Rodar o projeto em um terminal;
+// X Limpar o código inicial;
+// X Utilizar o useState pra criar uma variavel numérica que inicie em 0;
+// X Renderizar essa variável;
+// - Criar duas funções, uma pra subtrair um e uma pra adicionar um na variavel;
+// - Criar dois botões, um para cada função;
+// - Chamar as funções no onClick dos botões;
